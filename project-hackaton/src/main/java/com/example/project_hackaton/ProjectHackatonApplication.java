0de4d5c2be7d0f44a5891834dd47c419c7e144c9
Carlex27 <carlosocampo2711@gmail.com>
2024-07-13 17:29:49 -0500

    firts commit

---
+++ project-hackaton/src/main/java/com/example/project_hackaton/ProjectHackatonApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_hackaton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectHackatonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectHackatonApplication.class, args);
+	}
+
+}
---
+++ project-hackaton/src/test/java/com/example/project_hackaton/ProjectHackatonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project_hackaton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectHackatonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
