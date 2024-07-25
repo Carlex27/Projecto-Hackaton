431e7b6a315b429eb3e39d00dc5cd8930b37ceb1
Carlex27 <carlosocampo2711@gmail.com>
2024-07-24 20:41:20 -0500

    Update UserController.java

--- project-hackaton/src/main/java/com/example/project_hackaton/controller/UserController.java
+++ project-hackaton/src/main/java/com/example/project_hackaton/controller/UserController.java
@@ -2,6 +2,8 @@ package com.example.project_hackaton.controller;
 
 import com.example.project_hackaton.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -9,5 +11,14 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+
+    @PreAuthorize("hasAuthority('SCOPE_READ')")
+    @GetMapping("/all")
+    public ResponseEntity<?> allUsers(
+
+    ){
+        return ResponseEntity.ok(userService.findAll());
+    }
+    
 
 }
